c1d2c1d3f19edb34b6cb98623e9bbabaecdd5ffe
Paulo Gabriel <paulo.santos12@alunos.unis.edu.br>
2024-09-26 22:08:14 -0300

    AulaTeste

--- Atividade 8.c
+++ Atividade 8.c
@@ -1,31 +1,14 @@
 #include <stdio.h>
-
-struct Aluno {
-    char nome[50];
-    int idade;
-    float nota;
-};
+#include <string.h>
 
 int main() {
-    struct Aluno alunos[3];
+    char palavra[50];
+    printf("Digite uma palavra:");
+    fgets(palavra, 50, stdin);
 
-    for (int i = 0; i < 3; i++) {
-        printf("Digite o nome do aluno %d: ", i + 1);
-        scanf("%s", alunos[i].nome);
-        printf("Digite a idade do aluno %d: ", i + 1);
-        scanf("%d", &alunos[i].idade);
-        printf("Digite a nota do aluno %d: ", i + 1);
-        scanf("%f", &alunos[i].nota);
-    }
+    printf("A palavra foi %s\n", palavra);
+    int tamanho = strlen(palavra);
 
-
-    printf("\nInformações dos alunos:\n");
-    for (int i = 0; i < 3; i++) {
-        printf("Aluno %d:\n", i + 1);
-        printf("Nome: %s\n", alunos[i].nome);
-        printf("Idade: %d\n", alunos[i].idade);
-        printf("Nota: %.2f\n", alunos[i].nota);
-        printf("\n");
-    }
+    printf("A quantidade de letras é %d\n", tamanho-1);
     return 0;
 }
