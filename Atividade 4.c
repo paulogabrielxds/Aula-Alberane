dafd027798cee56c241656d7bff3b4473604b2e9
Paulo Gabriel <paulo.santos12@alunos.unis.edu.br>
2024-09-26 21:52:06 -0300

    AulaTeste

---
+++ Atividade 4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+    int main(void)
+{
+    int num;
+
+    printf("entrar com o numero\n:  ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0) {
+        printf("%d é um número par.\n", num);
+    } else {
+        printf("%d é um número ímpar.\n", num);
+    }
+
+
+    return 0;
