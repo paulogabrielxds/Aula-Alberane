3a92e4269854f2a972c155aedf5e5102b810fefc
Paulo Gabriel <paulo.santos12@alunos.unis.edu.br>
2024-09-26 22:09:55 -0300

    AulaTeste

--- Atividade 4.c
+++ Atividade 4.c
@@ -1,28 +1,18 @@
-struct Aluno {
-    char nome[50];
-    int idade;
-    float nota;
-};
+#include <stdio.h>
 
-int main() {
-    struct Aluno alunos[3];
+int main(void)
+{
+    int num;
 
-    for (int i = 0; i < 3; i++) {
-        printf("Digite o nome do aluno %d: ", i + 1);
-        scanf("%s", alunos[i].nome);
-        printf("Digite a idade do aluno %d: ", i + 1);
-        scanf("%d", &alunos[i].idade);
-        printf("Digite a nota do aluno %d: ", i + 1);
-        scanf("%f", &alunos[i].nota);
+    printf("entrar com o numero\n:  ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0) {
+        printf("%d é um número par.\n", num);
+    } else {
+        printf("%d é um número ímpar.\n", num);
     }
 
 
-    printf("\nComunicado ao alunos:\n");
-    for (int i = 0; i < 3; i++) {
-        printf("Aluno %d:\n", i + 1);
-        printf("Nome: %s\n", alunos[i].nome);
-        printf("Idade: %d\n", alunos[i].idade);
-        printf("Nota: %.2f\n", alunos[i].nota);
-        printf("\n");
-    }
+    return 0;
 
