fe9b3a787ec73fc42d52a06e7f20a9a6650cc398
Paulo Gabriel <paulo.santos12@alunos.unis.edu.br>
2024-09-26 21:25:50 -0300

    AulaTeste

--- main.c
+++ main.c
@@ -1,3 +1,21 @@
-//
-// Created by conta on 26/09/2024.
-//
+#include <stdio.h>
+
+int main(void) {
+    int a, b, c;
+    int soma;
+
+    printf("Digite um numero: ");
+    scanf("%d", &a);
+
+    printf("Digite outro numero: ");
+    scanf("%d", &b);
+
+    printf("Digite outro numero: ");
+    scanf("%d", &c);
+
+    soma = a + b + c;
+
+    printf("Soma Total: %d", soma);
+
+    return 0;
+}
